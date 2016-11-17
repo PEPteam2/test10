3de15e6eb9dc6ee1e0dc47a7346c92c7518c631b
lcl9288 <2450600633@qq.com>
2016-11-17 16:43:42 +0800

    V2.0

--- MyThread.java
+++ MyThread.java
@@ -8,6 +8,7 @@ package test13;
 public abstract class MyThread extends Thread {  
   
     private boolean suspend = false;  
+	private boolean suspend1=true;
   
     private String control = ""; // 只是需要一个对象而已，这个对象没有实际意义  
   
